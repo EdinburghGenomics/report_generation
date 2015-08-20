c75c9ce7923100d398c57cfda209080453e4e03d
tcezard <timothee.cezard@ed.ac.uk>
2015-08-20 16:35:44 +0100

    add test files glob the validation file instead of full path

--- bcbio_report.py
+++ bcbio_report.py
@@ -47,7 +47,7 @@ class Bcbio_report:
         total = sum(coverage_per_type.values())
         lib_info[LIBRARY_ELEMENT_PC_BASES_CALLABLE]= callable_bases/total
 
-        validation_files = os.path.join(bcbio_dir,'final', 'project_*', 'grading-summary-%s-joint.csv '%lib_name)
+        validation_files = glob.glob(os.path.join(bcbio_dir,'final', 'project_*', 'grading-summary-%s-joint.csv '%lib_name))
         if (validation_files)==1:
             snp_conc, indel_conc, snp_disc, indel_disc = parse_validate_csv(validation_files[0])
             lib_info[LIBRARY_ELEMENT_NB_SNP_CONCORDANT]= snp_conc
