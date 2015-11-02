a526464bc7cc7b4662ff119f263467a8d747a9c5
tcezard <timothee.cezard@ed.ac.uk>
2015-11-02 22:09:43 +0000

    Update to use Info object

--- bcbio_report.py
+++ bcbio_report.py
@@ -4,11 +4,11 @@ import glob
 import logging
 import os
 from report_generation.formaters import format_longint, format_percent, format_float
-from report_generation.model import Library_info, LIBRARY_ELEMENT_EXTERNAL_ID, LIBRARY_ELEMENT_NB_READS_SEQUENCED, \
-    LIBRARY_ELEMENT_NB_MAPPED_READS, LIBRARY_ELEMENT_NB_DUPLICATE_READS, LIBRARY_ELEMENT_NB_PROPERLY_MAPPED, \
-    LIBRARY_ELEMENT_MEDIAN_COVERAGE, LIBRARY_ELEMENT_NB_SNP_CONCORDANT, LIBRARY_ELEMENT_PC_SNP_CONCORDANT, \
-    LIBRARY_ELEMENT_NB_INDEL_CONCORDANT, LIBRARY_ELEMENT_PC_INDEL_CONCORDANT, LIBRARY_ELEMENT_PC_MAPPED_READS, \
-    LIBRARY_ELEMENT_PC_DUPLICATE_READS, LIBRARY_ELEMENT_PC_PROPERLY_MAPPED, LIBRARY_ELEMENT_PC_BASES_CALLABLE
+from report_generation.model import Info, ELEMENT_LIBRARY_EXTERNAL_ID, ELEMENT_NB_READS_SEQUENCED, \
+    ELEMENT_NB_MAPPED_READS, ELEMENT_NB_DUPLICATE_READS, ELEMENT_NB_PROPERLY_MAPPED, \
+    ELEMENT_MEDIAN_COVERAGE, ELEMENT_NB_SNP_CONCORDANT, ELEMENT_PC_SNP_CONCORDANT, \
+    ELEMENT_NB_INDEL_CONCORDANT, ELEMENT_PC_INDEL_CONCORDANT, ELEMENT_PC_MAPPED_READS, \
+    ELEMENT_PC_DUPLICATE_READS, ELEMENT_PC_PROPERLY_MAPPED, ELEMENT_PC_BASES_CALLABLE
 from report_generation.readers.mapping_stats_parsers import parse_bamtools_stats, parse_callable_bed_file, \
     parse_highdepth_yaml_file, parse_validate_csv
 
@@ -21,46 +21,48 @@ class Bcbio_report:
         self.bcbio_dirs=bcbio_dirs
 
     def _populate_lib_info(self, bcbio_dir):
-        lib_info = Library_info()
+        lib_info = Info()
         lib_name = get_library_name_from_dir(bcbio_dir)
         if lib_name is None:
             return None
 
         bamtools_path = os.path.join(bcbio_dir,'final', lib_name, 'qc','bamtools', 'bamtools_stats.txt')
         total_reads, mapped_reads, duplicate_reads, proper_pairs = parse_bamtools_stats(bamtools_path)
-        lib_info[LIBRARY_ELEMENT_EXTERNAL_ID]= lib_name
-        lib_info[LIBRARY_ELEMENT_NB_READS_SEQUENCED]= total_reads
-        lib_info[LIBRARY_ELEMENT_NB_MAPPED_READS]= mapped_reads
-        lib_info[LIBRARY_ELEMENT_PC_MAPPED_READS]= mapped_reads/float(total_reads)
-        lib_info[LIBRARY_ELEMENT_NB_DUPLICATE_READS]= duplicate_reads
-        lib_info[LIBRARY_ELEMENT_PC_DUPLICATE_READS]= duplicate_reads/float(mapped_reads)
-        lib_info[LIBRARY_ELEMENT_NB_PROPERLY_MAPPED]= proper_pairs
-        lib_info[LIBRARY_ELEMENT_PC_PROPERLY_MAPPED]= proper_pairs/float(mapped_reads)
+        lib_info[ELEMENT_LIBRARY_EXTERNAL_ID]= lib_name
+        lib_info[ELEMENT_NB_READS_SEQUENCED]= total_reads
+        lib_info[ELEMENT_NB_MAPPED_READS]= mapped_reads
+        lib_info[ELEMENT_NB_DUPLICATE_READS]= duplicate_reads
+        lib_info[ELEMENT_NB_PROPERLY_MAPPED]= proper_pairs
 
-        yaml_metric_path = os.path.join(bcbio_dir,'work', 'align', lib_name,'%s-sort-highdepth-stats.yaml'%lib_name)
-        median_coverage  = parse_highdepth_yaml_file(yaml_metric_path)
-        lib_info[LIBRARY_ELEMENT_MEDIAN_COVERAGE]= median_coverage
+        yaml_metric_paths = glob.glob(os.path.join(bcbio_dir,'work', 'align', lib_name,'*%s-sort-highdepth-stats.yaml'%lib_name))
+        if yaml_metric_paths:
+            yaml_metric_path = yaml_metric_paths[0]
+            median_coverage  = parse_highdepth_yaml_file(yaml_metric_path)
+            lib_info[ELEMENT_MEDIAN_COVERAGE]= median_coverage
 
-        bed_file_path = os.path.join(bcbio_dir,'work', 'align', lib_name,'%s-sort-callable.bed'%lib_name)
-        coverage_per_type = parse_callable_bed_file(bed_file_path)
-        callable_bases = coverage_per_type.get('CALLABLE')
-        total = sum(coverage_per_type.values())
-        lib_info[LIBRARY_ELEMENT_PC_BASES_CALLABLE]= callable_bases/total
-        validation_files = glob.glob(os.path.join(bcbio_dir,'final', 'project_*', 'grading-summary-%s-joint.csv'%lib_name))
+        bed_file_paths = glob.glob(os.path.join(bcbio_dir,'work', 'align', lib_name,'*%s-sort-callable.bed'%lib_name))
+        if bed_file_paths:
+            bed_file_path = bed_file_paths[0]
+            coverage_per_type = parse_callable_bed_file(bed_file_path)
+            callable_bases = coverage_per_type.get('CALLABLE')
+            total = sum(coverage_per_type.values())
+            lib_info[ELEMENT_PC_BASES_CALLABLE]= callable_bases/total
+
+        validation_files = glob.glob(os.path.join(bcbio_dir,'final', '*-merged', 'grading-summary-%s-joint.csv'%lib_name))
         if len(validation_files) == 1:
             snp_conc, indel_conc, snp_disc, indel_disc = parse_validate_csv(validation_files[0])
-            lib_info[LIBRARY_ELEMENT_NB_SNP_CONCORDANT]= snp_conc
-            lib_info[LIBRARY_ELEMENT_PC_SNP_CONCORDANT]= snp_conc/float(snp_conc+snp_disc)
-            lib_info[LIBRARY_ELEMENT_NB_INDEL_CONCORDANT]= indel_conc
-            lib_info[LIBRARY_ELEMENT_PC_INDEL_CONCORDANT]= indel_conc/float(indel_conc+indel_disc)
+            lib_info[ELEMENT_NB_SNP_CONCORDANT]= snp_conc
+            lib_info[ELEMENT_PC_SNP_CONCORDANT]= snp_conc/float(snp_conc+snp_disc)
+            lib_info[ELEMENT_NB_INDEL_CONCORDANT]= indel_conc
+            lib_info[ELEMENT_PC_INDEL_CONCORDANT]= indel_conc/float(indel_conc+indel_disc)
         return lib_info
 
     def _write_mapping_stats_report(self):
-        headers = [LIBRARY_ELEMENT_EXTERNAL_ID, LIBRARY_ELEMENT_NB_READS_SEQUENCED,LIBRARY_ELEMENT_NB_MAPPED_READS,
-                     LIBRARY_ELEMENT_NB_DUPLICATE_READS, LIBRARY_ELEMENT_PC_DUPLICATE_READS, LIBRARY_ELEMENT_NB_PROPERLY_MAPPED,
-                     LIBRARY_ELEMENT_PC_PROPERLY_MAPPED, LIBRARY_ELEMENT_MEDIAN_COVERAGE, LIBRARY_ELEMENT_PC_BASES_CALLABLE,
-                     LIBRARY_ELEMENT_NB_SNP_CONCORDANT, LIBRARY_ELEMENT_PC_SNP_CONCORDANT, LIBRARY_ELEMENT_NB_INDEL_CONCORDANT,
-                     LIBRARY_ELEMENT_PC_INDEL_CONCORDANT]
+        headers = [ELEMENT_LIBRARY_EXTERNAL_ID, ELEMENT_NB_READS_SEQUENCED,ELEMENT_NB_MAPPED_READS,
+                     ELEMENT_NB_DUPLICATE_READS, ELEMENT_PC_DUPLICATE_READS, ELEMENT_NB_PROPERLY_MAPPED,
+                     ELEMENT_PC_PROPERLY_MAPPED, ELEMENT_MEDIAN_COVERAGE, ELEMENT_PC_BASES_CALLABLE,
+                     ELEMENT_NB_SNP_CONCORDANT, ELEMENT_PC_SNP_CONCORDANT, ELEMENT_NB_INDEL_CONCORDANT,
+                     ELEMENT_PC_INDEL_CONCORDANT]
         table = []
         table.append('|| %s ||' % (' || '.join([str(h) for h in headers])))
         for bcbio_dir in self.bcbio_dirs:
